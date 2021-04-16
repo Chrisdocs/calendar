6c9d74d24ed1e9ee1c1ad76d2a0c2d8970b43ce9
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-16 17:42:43 -0500

    main display is complete, began experimenting with Luxon

---
+++ assets/javascript/script-cal.js
@@ -0,0 +1,106 @@
+let DateTime = luxon.DateTime;
+
+var dtfull = DateTime.fromObject({zone: 'America/Los_Angeles', numberingSystem: 'beng'})
+
+DateTime.fromISO("2017-05-15")          //=> May 15, 2017 at midnight
+DateTime.fromISO("2017-05-15T08:30:00") //=> May 15, 2017 at 8:30
+
+var dt = DateTime.now();
+// console.log(dt);
+
+//get specific components of the time; ie year, month, week, day, minute, second
+
+// var year = dt.year;
+// console.log("The year is " + year);
+// var month = dt.month;
+// console.log("The month is " + month);
+// var week = dt.weekday;
+// console.log("The day of the week is " + week);
+// var day = dt.day;
+// console.log("The day is " + day);
+// var hour = dt.hour;
+// console.log("The hour is " + hour);
+// var minute = dt.minute;
+// console.log("The minut is " + minute);
+// var second = dt.second;
+// console.log("the second is " + second);
+
+// other components you can call
+
+// var zone = dt.zoneName;
+// console.log(zone);
+// var offset = dt.offset;
+// console.log(offset);
+// var daysInMonth = dt.daysInMonth;
+// console.log(daysInMonth)
+
+// Formatting to humans to read
+
+var humantext = dt.toLocaleString();
+console.log(humantext);
+var humantext2 = dt.toLocaleString(DateTime.DATETIME_MED); //remove "TIME" to remove the time
+console.log(humantext2);
+
+var humantext3 = dt.toLocaleString(DateTime.DATE_MED);
+console.log("Today is " + humantext3);
+
+var plusa = dt.plus({ hours: 3, minutes: 2 });
+console.log(plusa);
+var minus = dt.minus({ days: 7 });
+console.log(minus);
+var startof = dt.startOf('day');
+console.log(startof);
+var endof = dt.endOf('hour');
+console.log(endof);
+
+var hoursArray = [
+    {hours: 0},
+    {hours: 1},
+    {hours: 2},
+    {hours: 3},
+    {hours: 4},
+    {hours: 5},
+    {hours: 6},
+    {hours: 7},
+    {hours: 8},
+    {hours: 9},
+    {hours: 10},
+    {hours: 11},
+    {hours: 12},
+    {hours: 13},
+    {hours: 14},
+    {hours: 15},
+    {hours: 16},
+    {hours: 17},
+    {hours: 18},
+    {hours: 19},
+    {hours: 20},
+    {hours: 21},
+    {hours: 22},
+    {hours: 23},
+];
+
+var arrayIndex = 0;
+function arrayToNode() {
+for (var i = 0; i < hoursArray.length; i++) {
+var zeroHour = startof.plus(hoursArray[i]).toLocaleString(DateTime.TIME_SIMPLE);
+var p = document.getElementsByClassName("cal-hr-p");
+
+}
+// while (arrayIndex < hoursArray.length) {
+//     console.log(hoursArray[arrayIndex])
+//     arrayIndex++;
+// }
+var allHours = document.querySelectorAll("p.cal-hr-p");
+console.log(allHours)
+NodeList.forEach(arrayToNode())
+}
+
+var today = document.getElementById("today-h2");
+today.textContent = "Hello, Today is " + humantext3;
+
+// var time0 = document.getElementById("p-hr-0");
+// time0.textContent = zeroHour;
+
+// var time1 = document.getElementById("p-hr-1");
+// time1.textContent = ""
